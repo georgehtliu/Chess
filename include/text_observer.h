89e8f14f873f4a63a261d3ee0630a75631093e61
frankljin <52306663+frankljin@users.noreply.github.com>
2021-12-02 15:04:09 -0500

    basic text observer

---
+++ include/text_observer.h
@@ -0,0 +1,22 @@
+#ifndef CHESS_INCLUDE_TEXT_OBSERVER_H_
+#define CHESS_INCLUDE_TEXT_OBSERVER_H_
+
+#include "board.h"
+#include "observer.h"
+#include "window.h"
+
+#include <memory>
+
+class TextObserver : public Observer {
+    Board *subject;
+    int top, bottom, left, right;
+
+    std::unique_ptr<Xwindow> w;
+
+   public:
+    TextObserver(Board *board);
+    void notify() override;
+    ~TextObserver();
+};
+
+#endif  // CHESS_INCLUDE_TEXT_OBSERVER_H_
