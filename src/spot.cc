fe0d5e2d90ea23bf4566b41356e5b12e729c5207
George Liu <georgeliu220@gmail.com>
2021-12-09 18:50:39 -0500

    add spot cc changes

--- src/spot.cc
+++ src/spot.cc
@@ -7,6 +7,7 @@
 #include "spot.h"
 
 Spot::Spot(int x, int y) : x{x}, y{y}, white{(x + y) % 2 == 0} {}
+Spot::Spot(int x, int y, Piece *p) : p{p}, x{x}, y{y}, white{(x + y) % 2 == 0} {}
 
 int Spot::get_x() const {
     return x;
@@ -18,4 +19,12 @@ int Spot::get_y() const {
 
 bool Spot::is_white() const {
     return white;
+}
+
+Piece* Spot::get_piece() const {
+    return p;
+}
+
+void Spot::set_piece(Piece *p) {
+    this->p = p;
 }
