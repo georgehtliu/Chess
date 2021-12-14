3f826b1356923c29c805367db84d2a122b8d7730
David Gurevich <david@gurevich.ca>
2021-12-14 00:50:20 -0500

    Fixed flipped rendering on GraphicsObserver

--- src/graphics_observer.cc
+++ src/graphics_observer.cc
@@ -44,7 +44,7 @@ void GraphicsObserver::notify() {
 
 void GraphicsObserver::notify(Spot *s) {
     int window_x = s->get_x() * GraphicsObserver::WIDTH + GraphicsObserver::BOARD_OFFSET;
-    int window_y = s->get_y() * GraphicsObserver::HEIGHT + GraphicsObserver::BOARD_OFFSET;
+    int window_y = (7 - s->get_y()) * GraphicsObserver::HEIGHT + GraphicsObserver::BOARD_OFFSET;
 
     int colour = (s->is_white()) ? GraphicsObserver::WHITE_COL : GraphicsObserver::BLACK_COL;
     w->fillRectangle(window_x, window_y, GraphicsObserver::WIDTH, GraphicsObserver::HEIGHT, colour);
